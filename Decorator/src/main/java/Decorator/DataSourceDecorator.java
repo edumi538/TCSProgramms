06d0e62c49ac788539049542e9cf66a8209d2136
Eduardo Miranda <edu.m.oliveira1@gmail.com>
2020-04-15 10:36:59 -0300

    Decorator

---
+++ Decorator/src/main/java/Decorator/DataSourceDecorator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorator;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class DataSourceDecorator implements DataSource {
+
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
